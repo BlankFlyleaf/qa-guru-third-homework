6be363e980159e5551d220b015cd714f8badf85a
BlankFlyleaf <kriechenrosenrot@gmail.com>
2025-02-05 14:33:34 +0300

    HomeWork Finished

--- src/test/java/thirdHomework.java
+++ src/test/java/thirdHomework.java
@@ -4,8 +4,10 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -16,22 +18,57 @@ public class thirdHomework {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
+        //Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void fillFormTest() {
-        open("/text-box");
-        $("#userName").setValue("Alex");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#currentAddress").setValue("Some street 1");
-        $("#permanentAddress").setValue("Another street 1");
+        open("/automation-practice-form");
+
+        //Fill String value
+        $("#firstName").setValue("Alexey");
+        $("#lastName").setValue("Churilov");
+        $("#userEmail").setValue("test@gmail.com");
+        $("#userNumber").setValue("89001112345");
+        $("#currentAddress").setValue("Moscow, Pushkin Street 23, Kolotushkin House, Apartament 32");
+        $("#subjectsInput").setValue("History").pressEnter();
+        $("#subjectsInput").setValue("Arts").pressEnter();
+
+        //Select Date of Birth
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").selectOption("1992");
+        $(".react-datepicker__day--021").click();
+
+        //Wrapper Button click
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+
+        //State and City click
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Agra")).click();
+
+        //Upload image
+        $("#uploadPicture").uploadFromClasspath("profilePic.jpg");
+
+        //Submit
         $("#submit").click();
 
-        $("#output #name").shouldHave(text("Alex"));
-        $("#output #email").shouldHave(text("alex@egorov.com"));
-        $("#output #currentAddress").shouldHave(text("Some street 1"));
-        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        //Output Check
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-body").shouldHave(text("Alexey Churilov"));
+        $(".modal-body").shouldHave(text("test@gmail.com"));
+        $(".modal-body").shouldHave(text("Male"));
+        $(".modal-body").shouldHave(text("8900111234"));
+        $(".modal-body").shouldHave(text("21 September,1992"));
+        $(".modal-body").shouldHave(text("History, Arts"));
+        $(".modal-body").shouldHave(text("Sports, Reading, Music"));
+        $(".modal-body").shouldHave(text("profilePic.jpg"));
+        $(".modal-body").shouldHave(text("Moscow, Pushkin Street 23, Kolotushkin House, Apartament 32"));
+        $(".modal-body").shouldHave(text("Uttar Pradesh Agra"));
     }
 }
