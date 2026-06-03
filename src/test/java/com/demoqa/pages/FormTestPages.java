1c3f643ba9ff213c7c715bca9b97f53cecbda2ca
BlankFlyleaf <kriechenrosenrot@gmail.com>
2026-06-03 09:59:34 +0300

    Удалил лишние комментарии Поправил метод outputCheckEmailError, ранее он проверял только наличие класса без его свойства.

--- src/test/java/com/demoqa/pages/FormTestPages.java
+++ src/test/java/com/demoqa/pages/FormTestPages.java
@@ -9,69 +9,69 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FormTestPages {
-    // valueElements
     private final SelenideElement firstNameInput = $("#firstName");
     private final SelenideElement lastNameInput = $("#lastName");
     private final SelenideElement emailInput = $("#userEmail");
     private final SelenideElement numberInput = $("#userNumber");
     private final SelenideElement currentAddressInput = $("#currentAddress");
     private final SelenideElement uploadPictureInput = $("#uploadPicture");
-
-    //clickValueElements
     private final SelenideElement sexInput = $("#genterWrapper");
     private final SelenideElement subjectInput = $("#subjectsInput");
     private final SelenideElement hobbiesInput = $("#hobbiesWrapper");
     private final SelenideElement stateCityInput = $("#stateCity-wrapper");
-
-    // clickActions
     private final SelenideElement formClick = $("#submit");
     private final SelenideElement calendarClick = $("#dateOfBirthInput");
     private final SelenideElement stateClick = $("#state");
     private final SelenideElement cityClick = $("#city");
 
-    // valueActions
     public FormTestPages typeFirstName(String value) {
         firstNameInput.setValue(value);
 
         return this;
     }
+
     public FormTestPages typeLastName(String value) {
         lastNameInput.setValue(value);
 
         return this;
     }
+
     public FormTestPages typeEmail(String value) {
         emailInput.setValue(value);
 
         return this;
     }
+
     public FormTestPages typeCurrentAddress(String value) {
         currentAddressInput.setValue(value);
 
         return this;
     }
+
     public FormTestPages typeNumber(String value) {
         numberInput.setValue(value);
 
         return this;
     }
+
     public FormTestPages uploadPicture(String value) {
         uploadPictureInput.uploadFromClasspath(value);
 
         return this;
     }
 
-    // clickAndUrlActions
     public FormTestPages submitForm(){
         formClick.click();
 
         return this;
     }
+
     public FormTestPages clickSex(String value) {
         sexInput.$(byText(value)).click();
 
         return this;
     }
+
     public FormTestPages clickHobbies(String value) {
         if (value != null) {
             hobbiesInput.$(byText(value)).click();
@@ -79,19 +79,19 @@ public class FormTestPages {
 
         return this;
     }
+
     public FormTestPages enterSubject(String value) {
         subjectInput.setValue(value).pressEnter();
 
         return this;
     }
+
     public FormTestPages openPage() {
         open("/automation-practice-form");
 
         return this;
     }
 
-
-    //componentActions
     public FormTestPages clickCalendar(String day, String month, String year) {
         calendarClick.click();
 
@@ -100,34 +100,37 @@ public class FormTestPages {
 
         return this;
     }
+
     public void setState (String value) {
         stateClick.click();
         stateCityInput.$(byText(value)).click();
     }
+
     public void setCity (String value) {
         cityClick.click();
         stateCityInput.$(byText(value)).click();
     }
+
     public FormTestPages clickAndStateInput(String state, String city) {
         setState(state);
         setCity(city);
         return this;
     }
 
-
-    // valueOutput
     public FormTestPages modalDialogAppear() {
         ModalResultComponent modalResultComponent = new ModalResultComponent();
         modalResultComponent.checkTitle("Thanks for submitting the form");
 
         return this;
     }
+
     public FormTestPages outputBodyValueCheck(String name, String value) {
         ModalResultComponent modalResultComponent = new ModalResultComponent();
         modalResultComponent.checkValue(name, value);
 
         return this;
     }
+
     public FormTestPages outputBodyHobbyCheck(String firstHobby, String secondHobby, String thirdHobby) {
         String expectedValue;
         ModalResultComponent modalResultComponent = new ModalResultComponent();
@@ -163,10 +166,12 @@ public class FormTestPages {
 
         return this;
     }
+
     public FormTestPages outputCheckError(String nameAttr, String nameElement, String value) {
         ModalResultComponent modalResultComponent = new ModalResultComponent();
         modalResultComponent.checkError(nameAttr, nameElement, value);
 
         return this;
     }
+
 }
--- src/test/java/com/demoqa/pages/SimpleFormTestPages.java
+++ src/test/java/com/demoqa/pages/SimpleFormTestPages.java
@@ -8,61 +8,58 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SimpleFormTestPages {
-    // valueElements
     private final SelenideElement userNameInput = $("#userName");
     private final SelenideElement userEmailInput = $("#userEmail");
     private final SelenideElement userCurrentAddressInput = $("#currentAddress");
     private final SelenideElement userPermanentAddressInput = $("#permanentAddress");
-
-    // clickActions
     private final SelenideElement clickInput = $("#submit");
-
-    // outputValue
     private final SelenideElement outputCheck = $("#output");
     private final SelenideElement emailOutput = $("#userEmail");
 
-    // clickAndUrlActions
     public SimpleFormTestPages openPage() {
         open("/text-box");
 
         return this;
     }
+
     public SimpleFormTestPages submitForm(){
         clickInput.click();
 
         return this;
     }
 
-    // valueActions
     public SimpleFormTestPages typeUserName(String value) {
         userNameInput.setValue(value);
 
         return this;
     }
+
     public SimpleFormTestPages typeUserEmail(String value) {
         userEmailInput.setValue(value);
 
         return this;
     }
+
     public SimpleFormTestPages typeUserCurrentAddress(String value) {
         userCurrentAddressInput.setValue(value);
 
         return this;
     }
+
     public SimpleFormTestPages typeUserPermanentAddress(String value) {
         userPermanentAddressInput.setValue(value);
 
         return this;
     }
 
-    // valueOutput
     public SimpleFormTestPages outputCheckValue(String value) {
         outputCheck.shouldHave(text(value));
 
         return this;
     }
+
     public SimpleFormTestPages outputCheckEmailError(String value, String s) {
-        emailOutput.shouldHave(attribute(value));
+        emailOutput.shouldHave(attribute(value, s));
 
         return this;
     }
