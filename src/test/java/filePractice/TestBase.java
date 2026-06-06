50cde02932424cbd6bd5db89b8cd69db481e3800
BlankFlyleaf <kriechenrosenrot@gmail.com>
2026-06-06 17:00:14 +0300

    Сделано 9 задание - 10

--- src/test/java/filePractice/TestBase.java
+++ src/test/java/filePractice/TestBase.java
@@ -1,9 +1,17 @@
 package filePractice;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import filePractice.utils.XlsHelper;
 import filePractice.utils.ZipHelper;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
     ZipHelper zipHelper = new ZipHelper();
     XlsHelper xlsHelper = new XlsHelper();
+
+    @BeforeAll
+    static void setUpAllure() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
 }
