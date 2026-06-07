7145145cd672b122f1770f815987b5d512856611
BlankFlyleaf <kriechenrosenrot@gmail.com>
2026-06-07 15:41:26 +0300

    Выполнил 11 задание

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -3,14 +3,17 @@ package com.demoqa.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
 import com.demoqa.pages.RegistrationFormPage;
 import com.demoqa.pages.SimpleFormPage;
 import com.github.javafaker.Faker;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Locale;
+import java.util.Map;
 
 import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
@@ -29,6 +32,14 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         //Configuration.holdBrowserOpen = true;
 
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                 "enableVNC", true,
+                    "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+
         //Configuration.browser = "chrome";
         //Configuration.browserVersion = "128.0";
         //Configuration.browserVersion = "130.0";
@@ -43,6 +54,13 @@ public class TestBase {
 
     @AfterEach
     void tearDown() {
+        step("Добавляем аттачи", () ->{
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+//        Attach.attachAsText("Some file", "Some content");
+        });
         step("Завершаем тест", Selenide::closeWebDriver);
     }
 }
